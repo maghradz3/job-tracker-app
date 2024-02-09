b069ef3571ab3c0ad17a0cc06fed6f863164053a
Levan Maghradze <120563893+maghradz3@users.noreply.github.com>
2024-02-09 17:31:12 +0400

    implement clerk authentication

---
+++ app/(dashboard)/add-job/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddJob = () => {
+  return <div className="text-4xl">AddJob</div>;
+};
+
+export default AddJob;
---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JobsPage = () => {
+  return <div className="text-4xl">JobsPage </div>;
+};
+
+export default JobsPage;
